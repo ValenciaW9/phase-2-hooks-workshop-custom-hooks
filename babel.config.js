2cfb0f0766587bc764af4f28a308da602d2064d2
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-12-30 00:03:39 -0500

    phase-2-hooks-workshop-custom-hooks
    phase-2-hooks-workshop-custom-hooks

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,9 @@
-module.exports = {
-  presets: [
-    "@babel/preset-env",
-    ["@babel/preset-react", { runtime: "automatic" }],
-  ],
+module.exports = function (api) {
+  api.cache(true);
+
+  const presets = ["@babel/preset-env", "@babel/preset-react"];
+
+  return {
+    presets,
+  };
 };
